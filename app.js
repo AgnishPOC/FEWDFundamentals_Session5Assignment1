9bcc3974d32ce9192891649cf0deb781216619c9
AgnishPOC <agnishshyam@outlook.com>
2016-10-31 11:25:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var name="Agnish";
+var age  = 27;
+var dob="19-06-1989"; // Date of Birth (String) is initialized
+var pob="Kolkata"; // Place of Birth (String) is initialized
+
+//Logging Details of the initialized variables
+
+console.log("Name : "+name+"\nAge : "+age+"\nDate of Birth : "+dob+"\nPlace of Birth : "+pob);
+alert("Please check console for details !!")
+
